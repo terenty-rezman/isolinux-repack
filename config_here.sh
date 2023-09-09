46b0859f737db179f2449e001a9cee596a96f76b
Terenty Rezman <terenty.rezman@mail.ru>
2023-09-09 16:11:10 +0300

    improve

--- config_here.sh
+++ config_here.sh
@@ -2,7 +2,9 @@
 INPUT_ISO="Boot_CD_R3_1_2_NEW_by_Королёв_Дополнительно_обработанный_Грузится.iso"
 OUTPUT_ISO="tsagi_rtfe_linux.iso"
 
-WORK_DIR="/tmp/custom_isolinux"
+cwd=$(pwd)
+
+WORK_DIR="${cwd}/extracted_files"
 ISO_MOUNT_DIR="${WORK_DIR}/mnt_iso"
 UNPACKED_ISO_DIR="${WORK_DIR}/unpacked_iso_edit_here" # <--- edit files in this dir! they will appear in the final iso!
 UNPACKED_INITRD_DIR="${WORK_DIR}/unpacked_initrd"
