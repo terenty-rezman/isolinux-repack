d1319cfb6a106ca7f29f1f51f8d9778eee2290a6
Terenty Rezman <terenty.rezman@mail.ru>
2023-09-10 22:22:18 +0300

    fix initrd unmount, fix profile

--- repack_iso.sh
+++ repack_iso.sh
@@ -8,11 +8,24 @@ source config_here.sh
 
 cwd=$(pwd)
 
+# unmount initrd first so modifications to files are flushed to disk
+sudo umount "$INITRD_MOUNT_DIR" || true
+sleep 1
+ 
+# then pack new initrd
 cd "$UNPACKED_INITRD_DIR"
 gzip -c initrd > initrd.bin
+
+# copy new initrd to iso dir
 sudo cp "${UNPACKED_INITRD_DIR}/initrd.bin" "${UNPACKED_ISO_DIR}/isolinux/initrd.bin"
+
+# create new iso
 cd "${UNPACKED_ISO_DIR}"
 sudo mkisofs -o ${cwd}/${OUTPUT_ISO} \
 	-b isolinux/isolinux.bin -c isolinux/boot.cat \
 	-no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "tsagi rtfe linux" \
 	.
+
+# mount initrd again
+cd "$UNPACKED_INITRD_DIR"
+sudo mount -o loop initrd "$INITRD_MOUNT_DIR"
